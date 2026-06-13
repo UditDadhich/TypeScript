67b473aab2a97f296c50b96cd8ee1dd3ce58e524
UditDadhich <dadhichudit2@gmail.com>
2026-06-13 16:26:08 +0530

    Initial typescript commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello ${person} Welcome to vs code`;
+}
+const username = "Udit Dadhich";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string): string{
+    return `Hello ${person} Welcome to vs code`
+}
+
+const username: string = "Udit Dadhich"
+console.log(greet(username))

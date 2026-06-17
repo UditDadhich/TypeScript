7deb90f67dddba5d111e3a9cdc3fed3abca0d1c6
UditDadhich <dadhichudit2@gmail.com>
2026-06-17 17:31:01 +0530

    objects

---
+++ src/objectTs.ts
@@ -0,0 +1,17 @@
+const chai = {
+    name : "Masala chai",
+    price: 20,
+    isHot: true
+}
+
+let tea : {
+    name: string;
+    price: number;
+    isHot: boolean
+}
+
+tea = {
+    name: "Ginger tea",
+    price: 25,
+    isHot: true
+}
